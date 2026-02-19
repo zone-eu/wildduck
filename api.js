cefb793e1e2b6d349f4d28a4a838020fb94b39e1
Nikolai Ovtsinnikov <nikolai@zone.ee>
2026-02-19 09:39:45 +0200

    for locks using single master node remove hash tags on namespace | 2

--- api.js
+++ api.js
@@ -561,7 +561,7 @@ module.exports = done => {
 
     server.lock = new Lock({
         redis: db.redis.isCluster ? db.redis.redisMaster : db.redis,
-        namespace: db.redis.isCluster ? '{mail}' : 'mail'
+        namespace: 'mail'
     });
 
     acmeRoutes(db, server, { disableRedirect: true });
