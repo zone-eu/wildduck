0cccce7fcfc68a7c340a55207548150ff7adeaf4
Nikolai Ovtsinnikov <nikolai@zone.ee>
2026-07-13 11:22:02 +0300

    quality of life fixes 2

--- test/api/metrics-test.js
+++ test/api/metrics-test.js
@@ -12,12 +12,10 @@ const server = supertest.agent(`http://127.0.0.1:${config.api.port}`);
 describe('Metrics API', function () {
     this.timeout(10000);
 
-    it.only('should GET /metrics expect success without access token', async () => {
+    it('should GET /metrics expect success without access token', async () => {
         await server.get('/health').expect(200);
 
         const response = await server.get('/metrics').expect(200);
-
-        console.log(response.text);
 
         expect(response.headers['content-type']).to.match(/^text\/plain/);
         expect(response.text).to.include('# HELP wildduck_info');
