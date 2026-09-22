15c43de34cbf43603b1ebace68e628e605169b2d
NickOvt <nikolai@zone.ee>
2026-09-22 10:32:35 +0300

    fix: ZMS-105: Fix search-apply task instability, now applies all changes (#1175)
    * fix: ZMS-105: Fix search-apply task instability, now applies are changes
    
    * batch by 2500 messages, continue from last _id
    
    * search-apply, add upper bound _id

--- test/search-apply-task-test.js
+++ test/search-apply-task-test.js
@@ -6,7 +6,45 @@ const { expect } = require('chai');
 const { ObjectId } = require('mongodb');
 
 const db = require('../lib/db');
+const consts = require('../lib/consts');
 const searchApplyTask = util.promisify(require('../lib/tasks/search-apply'));
+
+const createMessageIdCursor = (messageIds, filter, checkFilter) => {
+    const boundedFilter = filter.$and && filter.$and.length === 2 && filter.$and[1]._id && filter.$and[1]._id.$lte;
+    const baseFilter = boundedFilter ? filter.$and[0] : filter;
+    const idRange = boundedFilter ? filter.$and[1]._id : {};
+    let pageSize;
+    let sortDirection;
+
+    checkFilter(baseFilter);
+
+    return {
+        project(projection) {
+            expect(projection).to.deep.equal({ _id: true });
+            return this;
+        },
+        sort(sort) {
+            expect(sort._id).to.be.oneOf([-1, 1]);
+            sortDirection = sort._id;
+            return this;
+        },
+        limit(limit) {
+            expect(limit).to.equal(sortDirection === -1 ? 1 : consts.CURSOR_MAX_PAGE_SIZE);
+            pageSize = limit;
+            return this;
+        },
+        async toArray() {
+            if (sortDirection === -1) {
+                return messageIds.length ? [{ _id: messageIds[messageIds.length - 1] }] : [];
+            }
+
+            expect(idRange.$lte).to.deep.equal(messageIds[messageIds.length - 1]);
+            const start = idRange.$gt ? messageIds.findIndex(id => id.equals(idRange.$gt)) + 1 : 0;
+            const end = messageIds.findIndex(id => id.equals(idRange.$lte)) + 1;
+            return messageIds.slice(start, end).slice(0, pageSize).map(_id => ({ _id }));
+        }
+    };
+};
 
 describe('Search apply task', function () {
     it('should resolve move destination mailbox with the task user scope', async () => {
@@ -38,16 +76,9 @@ describe('Search apply task', function () {
                     case 'messages':
                         return {
                             find(filter) {
-                                expect(filter.user.toString()).to.equal(user.toString());
-
-                                return {
-                                    async next() {
-                                        return null;
-                                    },
-                                    async close() {
-                                        return;
-                                    }
-                                };
+                                return createMessageIdCursor([], filter, baseFilter => {
+                                    expect(baseFilter.user.toString()).to.equal(user.toString());
+                                });
                             }
                         };
 
@@ -93,5 +124,183 @@ describe('Search apply task', function () {
         expect(mailboxLookup).to.exist;
         expect(mailboxLookup.user.toString()).to.equal(user.toString());
         expect(mailboxLookup.mailbox.toString()).to.equal(destinationMailbox.toString());
+    });
+
+    it('should delete all 3000 messages matched by a 2025 date search', async () => {
+        const user = new ObjectId();
+        const mailbox = new ObjectId();
+        const messageIds = Array.from({ length: 3000 }, () => new ObjectId());
+        const deleted = [];
+
+        const originalUsers = db.users;
+        const originalDatabase = db.database;
+
+        db.users = {
+            collection(name) {
+                expect(name).to.equal('users');
+
+                return {
+                    async findOne() {
+                        return { _id: user };
+                    }
+                };
+            }
+        };
+
+        db.database = {
+            collection(name) {
+                expect(name).to.equal('messages');
+
+                return {
+                    find(filter) {
+                        return createMessageIdCursor(messageIds, filter, baseFilter => {
+                            expect(baseFilter.user.toString()).to.equal(user.toString());
+                            expect(baseFilter.idate.$gte).to.deep.equal(new Date('2025-01-01T00:00:00.000Z'));
+                            expect(baseFilter.idate.$lte).to.deep.equal(new Date('2025-12-31T23:59:59.999Z'));
+                        });
+                    },
+                    async findOne(query) {
+                        const _id = query._id;
+                        return {
+                            _id,
+                            user,
+                            mailbox,
+                            uid: messageIds.indexOf(_id) + 1,
+                            flags: [],
+                            size: 1
+                        };
+                    }
+                };
+            }
+        };
+
+        try {
+            await searchApplyTask(
+                { _id: new ObjectId() },
+                {
+                    user: user.toHexString(),
+                    mailbox: mailbox.toHexString(),
+                    datestart: new Date('2025-01-01T00:00:00.000Z'),
+                    dateend: new Date('2025-12-31T23:59:59.999Z'),
+                    action: { delete: true }
+                },
+                {
+                    messageHandler: {
+                        update(...args) {
+                            const callback = args[args.length - 1];
+                            callback(null, 0);
+                        },
+                        async delAsync(options) {
+                            deleted.push(options.messageData._id);
+                        }
+                    }
+                }
+            );
+        } finally {
+            db.users = originalUsers;
+            db.database = originalDatabase;
+        }
+
+        expect(deleted).to.deep.equal(messageIds);
+    });
+
+    it('should apply updates and moves to all 3000 matched messages', async () => {
+        const user = new ObjectId();
+        const mailbox = new ObjectId();
+        const destinationMailbox = new ObjectId();
+        const messageIds = Array.from({ length: 3000 }, () => new ObjectId());
+        const updated = [];
+        const moved = [];
+
+        const originalUsers = db.users;
+        const originalDatabase = db.database;
+
+        db.users = {
+            collection(name) {
+                expect(name).to.equal('users');
+
+                return {
+                    async findOne() {
+                        return { _id: user };
+                    }
+                };
+            }
+        };
+
+        db.database = {
+            collection(name) {
+                expect(name).to.equal('messages');
+
+                return {
+                    find(filter) {
+                        return createMessageIdCursor(messageIds, filter, baseFilter => {
+                            expect(baseFilter.user.toString()).to.equal(user.toString());
+                        });
+                    },
+                    async findOne(query, options) {
+                        expect(options).to.deep.equal({
+                            projection: {
+                                _id: true,
+                                user: true,
+                                mailbox: true,
+                                uid: true
+                            }
+                        });
+                        return {
+                            _id: query._id,
+                            user,
+                            mailbox,
+                            uid: messageIds.indexOf(query._id) + 1,
+                            flags: [],
+                            size: 1
+                        };
+                    }
+                };
+            }
+        };
+
+        try {
+            const messageHandler = {
+                update(...args) {
+                    updated.push(args[2]);
+                    const callback = args[args.length - 1];
+                    callback(null, 1);
+                },
+                async getMailboxAsync(query) {
+                    expect(query.user.toString()).to.equal(user.toString());
+                    expect(query.mailbox.toString()).to.equal(destinationMailbox.toString());
+                    return { _id: destinationMailbox };
+                },
+                async moveAsync(options) {
+                    moved.push(options.source.mailbox);
+                }
+            };
+
+            await searchApplyTask(
+                { _id: new ObjectId() },
+                {
+                    user: user.toHexString(),
+                    mailbox: mailbox.toHexString(),
+                    action: { seen: true }
+                },
+                { messageHandler }
+            );
+
+            await searchApplyTask(
+                { _id: new ObjectId() },
+                {
+                    user: user.toHexString(),
+                    mailbox: mailbox.toHexString(),
+                    action: { moveTo: destinationMailbox.toHexString() }
+                },
+                { messageHandler }
+            );
+        } finally {
+            db.users = originalUsers;
+            db.database = originalDatabase;
+        }
+
+        expect(updated).to.have.length(3000);
+        expect(moved).to.have.length(3000);
     });
 });
